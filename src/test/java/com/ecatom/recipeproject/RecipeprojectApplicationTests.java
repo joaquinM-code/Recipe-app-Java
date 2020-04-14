5f291cee2bca1fa6d83b15baa304b3870cc8cd87
ecatom <netpassrecovers@hotmail.com>
2020-04-14 16:02:11 +0200

    First commit

---
+++ src/test/java/com/ecatom/recipeproject/RecipeprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecatom.recipeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
