4f350489426ada8a49ecafb38f0560be7d9b335c
ecatom <netpassrecovers@hotmail.com>
2020-04-17 11:54:21 +0200

    Added getters and setters to Category

--- src/main/java/com/ecatom/recipeproject/domain/Category.java
+++ src/main/java/com/ecatom/recipeproject/domain/Category.java
@@ -12,4 +12,28 @@ public class Category {
 
     @ManyToMany(mappedBy = "categories")
     private Set<Recipe> recipes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
 }
